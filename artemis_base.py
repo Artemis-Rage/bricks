4b85d39562d6e876fb13dcf725734df9c30b18d7
ctrl-k <jamie.a.smith@gmail.com>
2025-09-27 20:37:45 -0400

    Added ArtemisBase, a DriveBase that tracks its position on the field.

---
+++ artemis_base.py
@@ -0,0 +1,63 @@
+"""ArtemisBase: A DriveBase that tracks its position on the field."""
+
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from umath import atan2, sqrt, pi
+import geometry
+
+hub = PrimeHub()
+
+hub.imu.reset_heading(0)
+
+
+class ArtemisBase(DriveBase):
+    """ArtemisBase a version of DriveBase that tracks its position on the field.
+    
+    Because ArtemisBase tracks its position, it can drive to specific (x, y)
+    coordinates on the field.
+    """
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.reset_position()
+
+    def reset_position(
+        self,
+        x: float = 0,
+        y: float = 0,
+    ):
+        self.x = x
+        self.y = y
+
+    def turn_to(
+        self,
+        heading: float,
+        then: Stop = Stop.HOLD,
+        wait: bool = True,
+    ):
+        """Turns the robot to face in the direction `heading`."""
+        heading = self.angle()
+        turn = (heading - heading) % 360
+        if turn > 180:
+            turn = turn - 360
+        self.turn(turn, then, wait)
+
+    def drive_to(
+        self,
+        x: float,
+        y: float,
+        then: Stop = Stop.HOLD,
+        wait: bool = True,
+    ):
+        """Drives from the current location to (x, y)."""
+        heading, distance = geometry.compute_trajectory(self.x, self.y, x, y)
+        self.turn_to(heading)
+        self.straight(distance, then=then, wait=wait)
+        self.reset_position(x, y)
---
+++ geometry.py
@@ -0,0 +1,26 @@
+"""Geometry utilities for robotics."""
+
+from umath import atan2, sqrt, pi
+
+def compute_trajectory(
+    x0: float,
+    y0: float,
+    x1: float,
+    y1: float,
+) -> tuple[float, float]:
+    """Given a current and target positions, compute the heading and distance.
+
+        Args:
+        x0: initial x position.
+        y0: initial y position.
+        x1: target x position.
+        y1: target y position.
+
+    Returns: a pair of values (heading, distance) representing the heading and
+      distance to drive to go from (x0, y0) to (x1, y1).
+    """
+    a = x1 - x0
+    b = y1 - y0
+    distance = sqrt(a**2 + b**2)
+    heading = umath.degrees(atan2(a, b))
+    return heading, distance
