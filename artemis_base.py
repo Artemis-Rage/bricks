d1e3d35e8f7ed67f3f9ebb0ff423c3b78fb02418
ctrl-k <jamie.a.smith@gmail.com>
2025-09-27 20:39:59 -0400

    Removes unused imports.

--- artemis_base.py
+++ artemis_base.py
@@ -1,11 +1,9 @@
 """ArtemisBase: A DriveBase that tracks its position on the field."""
 
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.parameters import Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-from umath import atan2, sqrt, pi
+
 import geometry
 
 hub = PrimeHub()
